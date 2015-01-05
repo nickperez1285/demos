69df0cb7981b4281e0590203b5501f4a25b99afe
Nick Perez <nickperez1285@gmail.com>
2015-01-04 20:02:36 -0800

    s

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 var app = {
-
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,7 +20,9 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        this.store = new MemoryStore(function(){
+            self.showAlert('Store Initialized', 'Info')
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
